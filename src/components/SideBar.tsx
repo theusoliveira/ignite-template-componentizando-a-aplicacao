024c87447b7b732837345c7947580ecad6f8a167
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2021-03-15 16:59:23 -0300

    Finish

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+import { GenreResponseProps } from '../App';
+
+interface SidebarProps {
+  genres: Array<GenreResponseProps>;
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar(props: SidebarProps) {
+  const { genres, selectedGenreId, handleClickButton } = props;
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
